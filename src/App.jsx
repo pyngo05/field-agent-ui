a5b6416ca13f2954103729e4559d1dd1e63f18cd
Yan Ngo <57730149+pyngo05@users.noreply.github.com>
2021-08-27 15:19:20 +0100

    delete works, update populates form but incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,10 @@ function App() {
   const [id2Edit, setId2Edit] = React.useState(null);
 
   // This is used to control the child component
-  //
   const [agent, setAgent] = React.useState({});
 
   // Array destructuring
   const [agents, setAgents] = React.useState([]);
-
   const [errors, setErrors] = React.useState([]);
 
   React.useEffect(
@@ -33,12 +31,6 @@ function App() {
     []
   );
 
-  // Const handleChange = (e) => {
-  //   // As we type in the input, we want to update the state
-  //   // This will updated the controlled Input component
-  //   setInputValue(e.target.value);
-  // };
-
   const handleFormChange = (event) => {
     // We update our agent as we type
     // When we submit, we send over agent state as the payload
@@ -50,7 +42,7 @@ function App() {
   };
 
   const handleClick = ({ target }) => {
-    const { dataset } = target;
+    const agentId = Number(target.dataset.agentId);
 
     // Get the button text - which button was clicked?
     switch (target.innerText.toLowerCase()) {
@@ -58,17 +50,16 @@ function App() {
         // We get the id from our data attribute in the button
         // We update state with this id
         // This will be checked when we submit so we can update, if necessary
-        setId2Edit(Number(dataset.agent));
+        setId2Edit(agentId);
 
         // How do we find the correct text to use?
-        setAgent(
-          agents.find(({ id }) => id === Number(dataset.agent)).description
-        );
+        setAgent(agents.find((agent) => agent.agentId === agentId));
+
         break;
       case "delete":
-        api.delete(dataset.agent).then(() => {
+        api.delete(agentId).then(() => {
           setAgents((prevAgents) =>
-            prevAgents.filter(({ id }) => id !== Number(dataset.agent))
+            prevAgents.filter((agent) => agent.agentId !== agentId)
           );
         });
 
@@ -82,72 +73,56 @@ function App() {
     // Do not refresh page when form is submitted
     e.preventDefault();
 
-    // Const form = e.target;
-    // const agentValue = form.elements[0].value.trim();
+    const form = e.target;
+    const agentValue = form.elements[0].value.trim();
 
-    // if (agentValue) {
-    // Check if this is an edit or a new agent
-    if (id2Edit) {
-      // We are updating
-      // api
-      //   .update({ id: id2Edit, description: agentValue })
-      //   .then(() => {
-      //     setAgents((prevAgents) =>
-      //       prevAgents.map((agent) => {
-      //         if (agent.id === id2Edit) {
-      //           // Avoid mutating the original agent object
-      //           // We create a new object by spreading out the original (...agent)
-      //           // We compose the new object with the updated properties
-      //           // `inputValue` is piece of state
-      //           const updatedAgent = { ...agent, description: inputValue };
-      //           return updatedAgent;
-      //         }
-      //         return agent;
-      //       })
-      //     );
-      //     // ⚠️ Don't get stuck in edit mode!
-      //     setId2Edit(null);
-      //     // Clear the input
-      //     setInputValue("");
-      //   })
-      //   .catch((err) => {
-      //     console.error("Some other error.", err);
-      //   });
-    }
-    // CREATE!
-    else {
-      api
-        .create(agent)
-        .then((response) =>
-          setAgents((prevAgents) => [...prevAgents, response])
-        )
-        .catch((errors) => setErrors(errors));
+    if (agentValue) {
+      // Check if this is an edit or a new agent
+      if (id2Edit) {
+        // We are updating
+        api
+          .update({ id: id2Edit, description: agent })
+          .then(() => {
+            setAgents((prevAgents) =>
+              prevAgents.map((agent) => {
+                if (agent.id === id2Edit) {
+                  // Avoid mutating the original agent object
+                  // We create a new object by spreading out the original (...agent)
+                  // We compose the new object with the updated properties
+                  // `inputValue` is piece of state
+                  const updatedAgent = { ...agent, description: agents };
+                  return updatedAgent;
+                }
 
-      setAgent({});
+                return agent;
+              })
+            );
+            // ⚠️ Don't get stuck in edit mode!
+            setId2Edit(null);
+            // Clear the input
+            setAgent("");
+          })
+          .catch((err) => {
+            console.error("Some other error.", err);
+          });
+      }
+      // CREATE!
+      else {
+        api
+          .create(agent)
+          .then((response) =>
+            setAgents((prevAgents) => [...prevAgents, response])
+          )
+          .catch((errors) => setErrors(errors));
+
+        setAgent({});
+      }
     }
-    // }
   };
 
   return (
     // Fragment tag
     <>
-      {/* <form onSubmit={handleSubmit} className="p-4"> */}
-      {/* Input gets re-rendered whenever inputValue changes. */}
-      {/* <Input value={inputValue} changeHandler={handleChange} />
-        <button
-          type="submit"
-          className="bg-green-500 ml-1 p-4 rounded-sm text-white my-2"
-        >
-          {id2Edit ? "Update" : "Add"} Agent
-        </button>
-      </form> */}
-
-      {/* private String firstName;
-    private String middleName;
-    private String lastName;
-    private LocalDate dob;
-    private int heightInInches; */}
-
       <form onSubmit={handleSubmit} className="p-4">
         {/* Input gets re-rendered whenever inputValue changes. */}
         <Input
@@ -157,18 +132,25 @@ function App() {
           label="First Name"
         />
 
-        {/* <Input
+        <Input
           value={agent.middleName}
           changeHandler={handleFormChange}
           name="middleName"
           label="Middle Name"
-        /> */}
+        />
 
         <Input
           value={agent.lastName}
           changeHandler={handleFormChange}
           name="lastName"
           label="Last Name"
+        />
+
+        <Input
+          value={agent.dob}
+          changeHandler={handleFormChange}
+          name="dob"
+          label="Date of Birth"
         />
 
         <Input
@@ -198,14 +180,14 @@ function App() {
             <button
               className="bg-yellow-500 ml-1 rounded-xl p-2"
               onClick={handleClick}
-              data-agent={agentId}
+              data-agent-id={agentId}
             >
               Update
             </button>
             <button
               className="bg-red-500 ml-1 rounded-xl text-white p-2"
               onClick={handleClick}
-              data-agent={agentId}
+              data-agent-id={agentId}
             >
               Delete
             </button>
